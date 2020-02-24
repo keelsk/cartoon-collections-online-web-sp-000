ae359a964ff8af02fe9c3eb6989c65c77957ad1c
Kirsten Keels <keelsk@gmail.com>
2020-02-24 22:21:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,10 @@ def long_planeteer_calls(calls) # code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese == cheese_types[i]
+  end
 end
